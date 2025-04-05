2c497135b9e61051f38c6bdcec879f95944ba0ea
ravenstudios <ravenstudiosrob@yahoo.com>
2025-04-04 22:49:28 -0500

    merge

--- menu_system.py
+++ menu_system.py
@@ -26,8 +26,8 @@ def system_menu(LCD):
     reboot_no = Menu_Object(title="No")
     reboot_menu = Menu_Object(title="Reboot", sub_menu=[reboot_yes, reboot_no])
 
-    brightness_up = Menu_Object(title="UP", py_command=lcd.brightness_up)
-    brightness_down = Menu_Object(title="UP", py_command=lcd.brightness_down)
+    brightness_up = Menu_Object(title="UP", py_command=lcd.brightness_up, comment="UP")
+    brightness_down = Menu_Object(title="DOWN", py_command=lcd.brightness_down, comment="DOWN")
     brightness = Menu_Object(title="Brightness", sub_menu=[brightness_up, brightness_down])
 
     system_menu.sub_menu = [
